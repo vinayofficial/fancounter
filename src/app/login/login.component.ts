778501835ed1d474b576efd9c60a51403830d984
Shakil Khan <38062331+shakil92@users.noreply.github.com>
2018-05-25 09:37:26 +0530

    login signup
    Done login signup With localstorage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  storageKey ="";
+  localData:any;
+  msg="";
+  signIn = new FormGroup({
+    userEmail : new FormControl('', Validators.required),
+    userPassword : new FormControl('', Validators.required)
+  });
+  
+  loginData(){
+       let useremail = this.signIn.get('userEmail').value;
+       let userpassword = this.signIn.get('userPassword').value;
+       this.storageKey = useremail;
+        let user = {
+        email:useremail,
+        password:userpassword
+      }
+
+      this.localData = localStorage.getItem(this.storageKey); // string
+      this.localData=JSON.parse(this.localData);  
+      console.log(this.localData);
+      console.log(this.localData.email +" && "+ useremail +" && "+ this.localData.paswword +" == "+userpassword);
+      if(this.localData.email == useremail && this.localData.paswword == userpassword){
+        this.msg="login Successfull";
+      }
+      else{
+        this.msg="Wrong credential";
+      }
+
+       
+
+  }
 
   constructor() { }
 
@@ -13,3 +45,29 @@ export class LoginComponent implements OnInit {
   }
 
 }
+
+
+
+
+// loginData(){
+//   let useremail = this.signIn.get('userEmail').value;
+//   let userpassword = this.signIn.get('userPassword').value;
+//   this.storageKey = useremail;
+//    let user = {
+//    email:useremail,
+//    password:userpassword
+//  }
+
+//    isLogin:boolean = false;
+
+//    this.loginService.checkLogin(user).subscribe(){
+//      let usermail = user.email;
+//      let password = user.password;
+     
+//      let userData = localStorage.getItem(user.name);
+     
+//      if(userData.email == usermail && userData.password == password){
+//        isLogin = true;
+//      }
+     
+//    }  
