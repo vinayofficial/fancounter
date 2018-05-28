481d10b9dd4391fa46945f12bf6d62235a44e2e3
shakil92 <skofficial92@gmail.com>
2018-05-28 11:25:50 +0530

    xyz

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  localData:any;
+  storageKey:any;
 
+  constructor() { }
+  // removeItem(){
+  //   localStorage.removeItem(cur);
+  // }
   ngOnInit() {
+    // this.storageKey = 
+    // this.localData = localStorage.getItem(this.storageKey);
+    // console.log(this.localData)
   }
 
 }
