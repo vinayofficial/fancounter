4b05b376fc35f86617046540f8807a83e1de7369
Shakil Khan <38062331+shakil92@users.noreply.github.com>
2018-06-02 08:29:46 +0530

    commit local

---
+++ src/assets/js/fierbase.js
@@ -0,0 +1,13 @@
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyATLmGaQZE6oop_NkzTKFjs9hwlswYUhCs",
+    authDomain: "fancounter-203804.firebaseapp.com",
+    databaseURL: "https://fancounter-203804.firebaseio.com",
+    projectId: "fancounter-203804",
+    storageBucket: "",
+    messagingSenderId: "620214597694"
+  };
+  firebase.initializeApp(config);
+</script>
